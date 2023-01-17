0d5e852756ff9b574ae4eab4f5dcedd5ad0ea355
Olukeye <seunolukeye2000@gmail.com>
2023-01-17 17:26:06 +0100

    added get all and single hotel endpoint

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -3,34 +3,58 @@ import Hotel  from "../models/hotels.js"
 
 
 const create_hotel = async (req, res) => {
-    
     const hotel = await new Hotel(req.body)
-    
     try {
-        const saveHotel = await hotel.save()
-        res.status(200).json(saveHotel)
-    } catch (e){
+        const savedHotel = await hotel.save()
+        res.status(200).json(savedHotel)
+    }catch (e){
         res.status(500).json(e);
     }
 }
 
 
 const update_hotel = async (req, res) => {
-
-    const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
-    
+    const updateHotel = await Hotel.findByIdAndUpdate(req.params.id,
+        { $set: req.body }, { new: true });
     try {
         res.status(200).json(updateHotel)
-    } catch (e) {
+    }catch (e) {
         res.status(404).json(e)
     }
 }
 
 
+const delete_hotel = async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Deleted successfully!");
+    }catch (e) {
+        res.status(500).json(e)
+    }
+}
 
+const get_single_hotel = async (req, res) => {
+    try {
+        const singleHotel = await Hotel.findById(req.params.id)
+        res.status(200).json(singleHotel)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
 
+const get_all_hotels = async (req, res) => {
+    try {
+        const allHotels = await Hotel.find()
+        res.status(200).json(allHotels.reverse())
+    } catch (e) {
+        res.status(500).json(e)
+    }
+}
 
 export {
     create_hotel,
-    update_hotel
+    update_hotel,
+    delete_hotel,
+    get_single_hotel,
+    get_all_hotels
 }
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { create_hotel, update_hotel } from "../controllers/hotels.js";
+import { create_hotel, update_hotel, delete_hotel, get_single_hotel, get_all_hotels } from "../controllers/hotels.js";
 const router = express.Router();
 
 
 router.post('/', create_hotel);
 router.put('/:id', update_hotel);
+router.delete('/:id', delete_hotel);
+router.get('/:id', get_single_hotel);
+router.get('/', get_all_hotels)
 
 
 export default router;
